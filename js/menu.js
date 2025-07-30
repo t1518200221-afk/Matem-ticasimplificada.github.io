2c900986690d3df483d5aa10c41ce2e7366fd7f8
t1518200221-afk <t1518200221@unitru.edu.pe>
2025-07-29 20:15:12 -0500

    Add files via upload

---
+++ js/menu.js
@@ -0,0 +1,17 @@
+8function)(){
+    const openButton = document.querySelector('.nav__menu');
+    const menu = document.querySelector('nav__link');
+    const closeMenú =document.querySelector('navigator.__close');
+
+    openButton.addEventListener('click,()=>{
+        menu.classList.remove('nav__link--show');
+});
+
+
+
+
+
+
+
+
+})();
